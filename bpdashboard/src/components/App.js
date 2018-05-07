dec2bef45cb23a1153a4726f7a955fa2c6fd681b
oystpoyst <oystein.hammersland@gmail.com>
2018-05-07 17:53:42 +0200

    [Feature] Added input fields and button to login component. And a minor animation to the logo - TKDATA-221

--- bpdashboard/src/components/App.js
+++ bpdashboard/src/components/App.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
 import logo from './car.png';
 import './../styles/App.css';
-
+import Login from './login/Login';
 class App extends Component {
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Bilpark Dashboard</h1>
+          <h1 className="App-title">Velkommen til Bilparken</h1>
         </header>
-        <p className="App-intro">
-          <code>place login here</code>
-        </p>
+        <div className="App-intro">
+          <Login/>
+        </div>
       </div>
     );
   }
